4f4213889adaeb28d62ffafb6811dc3f240242d2
Rahul-A-Verma <rv5070782@gmail.com>
2024-07-09 15:30:00 +0530

    update style

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -15,7 +15,7 @@ const Meal = () => {
                 <div className="searchBox">
                     <input type="search" className="search-bar" onChange={(e)=>setSearch(e.target.value)} value={search} />
                     <button
-            className="bg-red-950 text-white rounded-md px-4 py-2 ml-2 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
+            className="bg-red-950 text-white rounded-full px-4 py-2 ml-2 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
             onClick={()=>searchMeal(search)}>
             Search
         </button>
